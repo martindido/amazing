2ed7ea40fc93da73a5a7264860acc11dde19f52c
Martin Di Domenico <martindido87@gmail.com>
2014-12-09 00:24:48 -0300

    Canvas

--- app/js/app.js
+++ app/js/app.js
@@ -2,63 +2,78 @@ var App = (function() {
     'use strict';
 
     function App() {
-        this.maze = new Maze(5, 10, Maze.Algorithms['Eller']);
-        this.$maze = $('#maze');
         this.$score = $('#score');
+        this.getSize(5);
+        this.maze = new Maze(this.size.x, this.size.y, Maze.Algorithms['Eller']);
         this.score = 0;
-        this.interval = 30;
+        this.interval = 300;
         this.cells = {};
-        this.current;
-        this.world = new World();
+        this.world = new World(this.size.width, this.size.height);
+        this.steps = 0;
+        this.current = 0;
 
         _.each(this.maze.grid.data, this.generateRow, this);
 
         this.maze.onUpdate(this.onUpdate.bind(this));
         this.maze.onEvent(this.onEvent.bind(this));
-        this.maze.algorithm.isFinal = this.isFinal;
+        this.maze.algorithm.isFinal = this.isFinal.bind(this);
+    }
+
+    App.prototype.getSize = function(x) {
+        this.size = {
+            x: x,
+            width: $('body').width(),
+            height: $('body').height()
+        };
+
+        this.size.cellWidth = this.size.width / this.size.x;
+        this.size.cellHeight = this.size.cellWidth;
+        this.size.y = Math.ceil(this.size.height / this.size.cellHeight) + 2;
     }
 
     App.prototype.generateRow = function(row, y) {
-        var $row = $(JST['app/templates/row.html']({
-            y: y
-        }));
-
         this.cells[y] = {};
         _.each(row, function each(cell, x) {
-            var $cell = $(JST['app/templates/cell.html']({
-                x: x,
-                y: y
-            }));
-
-            this.cells[y][x] = $cell;
-            $row.prepend($cell);
+            this.cells[y][x] = {};
+            _.each(_.range(3), function each(i) {
+                this.cells[y][x][i] = {};
+                _.each(_.range(3), function each(j) {
+                    if (i === 1 && j === 1) {
+                        return;
+                    }
+                    this.cells[y][x][i][j] = this.world.addSquare({
+                        x: Math.round(this.size.cellWidth * x + this.size.cellWidth / 3 * j + this.size.cellWidth / 6),
+                        y: Math.round(this.size.height - (this.size.cellHeight * y + this.size.cellHeight / 3 * i + this.size.cellHeight / 6)),
+                        width: this.size.cellWidth / 3,
+                        height: this.size.cellHeight / 3,
+                        styles: {
+                            fillStyle: x % 2 ? '#FF0000' : '#0000FF',
+                            lineWidth: 0
+                        }
+                    });
+                }, this);
+            }, this);
         }, this);
-        this.$maze.prepend($row);
     }
 
     App.prototype.onUpdate = function(maze, x, y) {
-        var $cell = this.cells[y][x];
+        var cell = this.cells[y][x];
 
         if (this.maze.isEast(x, y)) {
-            $cell.addClass('east');
+            this.world.removeSquare(cell[1][2]);
         }
         if (this.maze.isWest(x, y)) {
-            $cell.addClass('west');
+            this.world.removeSquare(cell[1][0]);
         }
         if (this.maze.isNorth(x, y)) {
-            $cell.addClass('north');
+            this.world.removeSquare(cell[0][1]);
         }
         if (this.maze.isSouth(x, y)) {
-            $cell.addClass('south');
-        }
-        if (this.maze.isUnder(x, y)) {
-            $cell.addClass('under');
+            this.world.removeSquare(cell[2][1]);
         }
     }
 
     App.prototype.onEvent = function(maze, x, y) {
-        var firstRow;
-
         if (this.current === y) {
             return;
         }
@@ -66,29 +81,53 @@ var App = (function() {
         if (y < this.maze.height - 1) {
             return;
         }
+        if (y === this.maze.height - 1) {
+            this.world.startCamera(this.onStep.bind(this));
+        }
+    }
+
+    App.prototype.isFinal = function() {
+        this.paused = this.current == Object.keys(this.cells).pop();
+        return false;
+    }
+
+    App.prototype.start = function() {
+        this.world.start({
+            circle: {
+                x: this.size.width / 2,
+                y: this.size.height - this.size.cellHeight / 2,
+                radius: 10
+            },
+            onTick: this.onTick.bind(this)
+        });
+        this.intervalId = setInterval(this.onInterval.bind(this), this.interval);
+    }
+
+    App.prototype.onTick = function() {
+        if (!this.paused) {
+            this.maze.step();
+        }
+    }
+
+    App.prototype.onInterval = function() {
+        if (this.world.speed > 5) {
+            this.world.speed--;
+        }
+    }
+
+    App.prototype.onStep = function() {
+        var firstRow = Object.keys(this.cells).shift();
+
         this.score += this.maze.width + this.current;
         this.$score.text(this.score);
         this.maze.grid.data.push(_.range(this.maze.width).map(function each() {
             return 0;
         }));
-        this.generateRow(this.maze.grid.data[y + 1], y + 1);
-        firstRow = Object.keys(this.cells).shift();
-        this.cells[firstRow][0].parent().remove();
+        this.generateRow(this.maze.grid.data[this.current + 1], this.current + 1);
         delete this.cells[firstRow];
-    }
-
-    App.prototype.isFinal = function() {
-        return false;
-    }
-
-    App.prototype.start = function() {
-        this.intervalId = setInterval(this.onInterval.bind(this), this.interval);
-    }
-
-    App.prototype.onInterval = function() {
-        if (!this.maze.step()) {
-            clearInterval(this.intervalId);
-        }
+        delete this.maze.grid.data[firstRow];
+        this.world.removeSquares();
+        this.paused = false;
     }
 
     return App;
--- app/js/world.js
+++ app/js/world.js
@@ -1,12 +1,140 @@
 var World = (function() {
     'use strict';
 
-    function World() {
+    function World(width, height) {
         this.physics = Physics();
+        this.width = width;
+        this.height = height;
+        this.behaviors = {};
+        this.bodies = {};
+        this.squares = [];
+        this.delta = 0;
+        this.speed = 50;
+
+        this.addRenderer();
+        this.addBehaviors();
     }
 
-    World.prototype.isFinal = function() {
-        return false;
+    World.prototype.addRenderer = function() {
+        this.renderer = Physics.renderer('canvas', {
+            id: 'maze',
+            width: this.width,
+            height: this.height,
+            styles: {
+                circle: {
+                    strokeStyle: '#351024',
+                    lineWidth: 1,
+                    fillStyle: '#b58900',
+                    angleIndicator: '#351024'
+                }
+            },
+            offset: {
+                x: 0,
+                y: 350
+            }
+        });
+        this.physics.add(this.renderer);
+        this.physics.on('step', this.physics.render.bind(this.physics));
+    }
+
+    World.prototype.addCircle = function(options) {
+        var circle = Physics.body('circle', _.extend(options));
+
+        this.bodies.circle = circle;
+        this.physics.add(this.bodies.circle);
+        return circle;
+    }
+
+    World.prototype.addSquare = function(options) {
+        var square = this.squares.pop();
+
+        if (!square) {
+            square = Physics.body('rectangle', _.extend(options, {
+                treatment: 'static',
+                restitution: 0
+            }));
+        }
+        else {
+            square.state.pos.set(options.x, options.y);
+        }
+        this.bodies.squares = this.bodies.squares || [];
+        this.bodies.squares.push(square);
+        this.physics.add(square);
+        return square;
+    }
+
+    World.prototype.removeSquare = function(square) {
+        this.physics.remove(square);
+    }
+
+    World.prototype.addBehaviors = function() {
+        this.detectEdgeCollisions();
+        this.impulseBodyResponse();
+        this.detectBodyCollisions();
+        this.gravity();
+    }
+
+    World.prototype.detectEdgeCollisions = function() {
+        this.bounds = Physics.aabb(0, 0, this.width, this.height);
+        this.behaviors.edgeCollisionDetection = Physics.behavior('edge-collision-detection', {
+            aabb: this.bounds,
+            restitution: 0,
+            cof: 0
+        });
+        this.physics.add(this.behaviors.edgeCollisionDetection);
+    }
+
+    World.prototype.detectBodyCollisions = function() {
+        this.behaviors.bodyCollisionDetection = Physics.behavior('body-collision-detection');
+        this.behaviors.sweepPrune = Physics.behavior('sweep-prune');
+        this.physics.add(this.behaviors.bodyCollisionDetection);
+        this.physics.add(this.behaviors.sweepPrune);
+
+    }
+
+    World.prototype.impulseBodyResponse = function() {
+        this.behaviors.bodyImpulseResponse = Physics.behavior('body-impulse-response');
+        this.physics.add(this.behaviors.bodyImpulseResponse);
+    }
+
+    World.prototype.gravity = function() {
+        this.behaviors.constantAcceleration = Physics.behavior('constant-acceleration');
+        this.physics.add(this.behaviors.constantAcceleration);
+    }
+
+    World.prototype.onTick = function(time, dt) {
+        this.physics.step(time);
+    }
+
+    World.prototype.start = function(options) {
+        this.addCircle(options.circle);
+        this.ticker = Physics.util.ticker;
+        this.ticker.on(this.onTick.bind(this));
+        this.ticker.on(options.onTick);
+        this.ticker.start();
+    }
+
+    World.prototype.startCamera = function(onStep) {
+        this.physics.on('step', this.moveCamera.bind(this, onStep));
+        this.renderer._layers.main.options.offset = Physics.vector(0, 0);
+    }
+
+    World.prototype.moveCamera = function(onStep) {
+        var square = _.first(this.bodies.squares);
+        var delta = square.height / this.speed;
+
+        this.delta += delta;
+        this.renderer._layers.main.options.offset.vsub(Physics.vector(0, -delta));
+        if (this.delta >= square.height * 3) {
+            this.delta = 0;
+            onStep();
+        }
+    }
+
+    World.prototype.removeSquares = function() {
+        this.squares = this.bodies.squares.slice(0, 40);
+        this.physics.remove(this.squares);
+        this.bodies.squares = this.bodies.squares.slice(40);
     }
 
     return World;
